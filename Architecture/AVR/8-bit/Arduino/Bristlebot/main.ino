6f9e82f433d64e1447cfa065f11fb4f599a8ed88
Jozef Chovan <jozef.chovan@gmail.com>
2021-03-21 13:37:47 +0100

    Add an auto stop feature to Bristlebot.

--- Architecture/AVR/8-bit/Arduino/Bristlebot/main.ino
+++ Architecture/AVR/8-bit/Arduino/Bristlebot/main.ino
@@ -3,6 +3,7 @@
 
 #define MOTOR_DRIVER 9
 IRrecv receiver(11);
+int speed = 0;
 
 void setup()
 {
@@ -16,22 +17,49 @@ void loop()
         switch (receiver.results.value) {
         case 0xE0E040BF: // Turn OFF
             analogWrite(MOTOR_DRIVER, 0);
+            speed = 0;
             break;
 
         case 0xE0E020DF: // Speed 1
             analogWrite(MOTOR_DRIVER, 100);
+            speed = 1;
             break;
 
         case 0xE0E0A05F: // Speed 2
             analogWrite(MOTOR_DRIVER, 150);
-            break;
-
-        case 0xE0E0609F: // Speed 3
-            analogWrite(MOTOR_DRIVER, 200);
+            speed = 2;
             break;
         }
 
-        delay(250);
+        delay(1000);
         receiver.resume();
+    }
+
+    stopMotorIfStuck(speed);
+    delay(50);
+}
+
+void stopMotorIfStuck(int speed)
+{
+    switch (speed) {
+    case 1:
+        if (analogRead(A3) < 30) {
+            delay(1000);
+            if (analogRead(A3) < 30) {
+                analogWrite(MOTOR_DRIVER, 0);
+                speed = 0;
+            }
+        }
+        break;
+
+    case 2:
+        if (analogRead(A3) < 57) {
+            delay(1000);
+            if (analogRead(A3) < 57) {
+                analogWrite(MOTOR_DRIVER, 0);
+                speed = 0;
+            }
+        }
+        break;
     }
 }
