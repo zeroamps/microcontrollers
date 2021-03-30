b9b6e200ad684a8b5ec2991de0cd01424aab9e75
Jozef Chovan <jozef.chovan@gmail.com>
2021-03-30 16:40:23 +0200

    Update the Carbot turning speed.

--- Architecture/AVR/8-bit/Arduino/Carbot/main.ino
+++ Architecture/AVR/8-bit/Arduino/Carbot/main.ino
@@ -2,6 +2,8 @@
 #include <IRremote.h>
 #include <MotorDriver.h>
 
+#define SPEED1 255
+#define SPEED2 153
 #define DELAY 50
 
 IRrecv receiver(9);
@@ -37,37 +39,37 @@ void loop()
 
 void forward()
 {
-    motorDriver.motor(1, FORWARD, 255);
-    motorDriver.motor(2, FORWARD, 255);
-    motorDriver.motor(3, FORWARD, 255);
-    motorDriver.motor(4, FORWARD, 255);
+    motorDriver.motor(1, FORWARD, SPEED1);
+    motorDriver.motor(2, FORWARD, SPEED1);
+    motorDriver.motor(3, FORWARD, SPEED1);
+    motorDriver.motor(4, FORWARD, SPEED1);
     delay(DELAY);
 }
 
 void backward()
 {
-    motorDriver.motor(1, BACKWARD, 255);
-    motorDriver.motor(2, BACKWARD, 255);
-    motorDriver.motor(3, BACKWARD, 255);
-    motorDriver.motor(4, BACKWARD, 255);
+    motorDriver.motor(1, BACKWARD, SPEED1);
+    motorDriver.motor(2, BACKWARD, SPEED1);
+    motorDriver.motor(3, BACKWARD, SPEED1);
+    motorDriver.motor(4, BACKWARD, SPEED1);
     delay(DELAY);
 }
 
 void left()
 {
-    motorDriver.motor(1, FORWARD, 255);
-    motorDriver.motor(2, BACKWARD, 255);
-    motorDriver.motor(3, BACKWARD, 255);
-    motorDriver.motor(4, FORWARD, 255);
+    motorDriver.motor(1, FORWARD, SPEED2);
+    motorDriver.motor(2, BACKWARD, SPEED2);
+    motorDriver.motor(3, BACKWARD, SPEED2);
+    motorDriver.motor(4, FORWARD, SPEED2);
     delay(DELAY);
 }
 
 void right()
 {
-    motorDriver.motor(1, BACKWARD, 255);
-    motorDriver.motor(2, FORWARD, 255);
-    motorDriver.motor(3, FORWARD, 255);
-    motorDriver.motor(4, BACKWARD, 255);
+    motorDriver.motor(1, BACKWARD, SPEED2);
+    motorDriver.motor(2, FORWARD, SPEED2);
+    motorDriver.motor(3, FORWARD, SPEED2);
+    motorDriver.motor(4, BACKWARD, SPEED2);
     delay(DELAY);
 }
 
