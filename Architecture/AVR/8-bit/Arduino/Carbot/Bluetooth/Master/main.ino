647e721915796eb730e1c2089051c0da4025b309
Jozef Chovan <jozef.chovan@gmail.com>
2021-04-17 10:17:19 +0200

    Update the bluetooth control for Carbot.

--- Architecture/AVR/8-bit/Arduino/Carbot/Bluetooth/Master/main.ino
+++ Architecture/AVR/8-bit/Arduino/Carbot/Bluetooth/Master/main.ino
@@ -1,7 +1,12 @@
 #include <Arduino.h>
 
+#define LBUTTON 4
+#define RBUTTON 3
+#define TBUTTON 2
+#define BBUTTON 5
+
 #define X A1
-#define Y A0 
+#define Y A0
 
 int x = 0;
 int y = 0;
@@ -9,6 +14,11 @@ int y = 0;
 void setup()
 {
     Serial.begin(115200);
+
+    pinMode(LBUTTON, INPUT_PULLUP);
+    pinMode(RBUTTON, INPUT_PULLUP);
+    pinMode(TBUTTON, INPUT_PULLUP);
+    pinMode(BBUTTON, INPUT_PULLUP);
 }
 
 void loop()
@@ -26,6 +36,20 @@ void loop()
         Serial.write('F');
         delay(50);
     } else if (y > 600) {
+        Serial.write('B');
+        delay(50);
+    }
+
+    if (digitalRead(LBUTTON) == LOW) {
+        Serial.write('L');
+        delay(50);
+    } else if (digitalRead(RBUTTON) == LOW) {
+        Serial.write('R');
+        delay(50);
+    } else if (digitalRead(TBUTTON) == LOW) {
+        Serial.write('F');
+        delay(50);
+    } else if (digitalRead(BBUTTON) == LOW) {
         Serial.write('B');
         delay(50);
     }
